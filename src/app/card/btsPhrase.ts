bdc31bce25c3515cbe9bbaceab7c90603138da06
Glendy Tuyuc <glendytuyuc@gmail.com>
2025-03-27 19:19:44 -0600

    first commit

---
+++ src/app/card/btsPhrase.ts
@@ -0,0 +1,50 @@
+import { BtsPhrases } from "@/types";
+
+
+export const btsPhrase: BtsPhrases[] = [
+  {
+      title: "our Eternal youth",
+      from: "BTS",
+      image: "/bg (1).avif"
+  },
+  {
+      title: "our Biggest voice",
+      from: "BTS",
+      image: "/bg2.avif"
+  },
+  {
+      title: "the most beautiful",
+      from: "BTS",
+      image: "/bg3.avif"
+  },
+  {
+      title: "our Chapter 1",
+      from: "BTS",
+      image: "/bg4.avif"
+  },
+  {
+      title: "our Happines",
+      from: "BTS",
+      image: "/bg5.avif"
+  },
+  {
+      title: "the Biggest love",
+      from: "BTS",
+      image: "/bg6.avif"
+  },
+  {
+      title: "our Universe",
+      from: "BTS",
+      image: "/bg7.webp"
+  },
+  {
+      title: "Sweetest",
+      from: "BTS",
+      image: "/bg8.webp"
+  },
+  {
+      title: "our Wings",
+      from: "BTS",
+      image: "/bg9.webp"
+  },
+];
---
+++ src/types/index.ts
@@ -0,0 +1,5 @@
+export type BtsPhrases = {
+  title: string;
+  from: string;
+  image: string;
+};
