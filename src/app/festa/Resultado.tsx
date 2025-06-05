96400132f95893c88783db715ae067bccd452f79
Glendy Tuyuc <glendytuyuc@gmail.com>
2025-06-04 18:30:01 -0600

    fixing rotate

--- src/app/festa/Resultado.tsx
+++ src/app/festa/Resultado.tsx
@@ -31,14 +31,15 @@ export default function Resultado() {
         )}
 
         <div
-          className={`absolute inset-0 flex flex-col font-extrabold items-center justify-end  ${
+          className={`absolute inset-0 flex flex-col font-extrabold items-center justify-end  rotate ${
             isMobile ? "pb-16" : "pb-[5rem]"
-          }`}
+          }`}  style={{ transform: "rotate(-12deg)" }}
         >
           <div
             className={`flex flex-col items-center justify-center gap-1 bg-white/90 rounded-xs px-1 ${selectedStyle?.color} `}
           >
-            {name} from {content}
+            <span>{name}</span>
+             from {content}
           </div>
         </div>
       </div>
