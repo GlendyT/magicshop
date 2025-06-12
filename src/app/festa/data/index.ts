7c40e41f4b4f1b7671ff62cf34e916c7efa20da9
Glendy Tuyuc <glendytuyuc@gmail.com>
2025-06-12 13:43:21 -0600

    adding the images for suga

--- src/app/festa/data/index.ts
+++ src/app/festa/data/index.ts
@@ -1,7 +1,7 @@
 import type { FestaBtsProps } from "@/types/index";
 
 export const festaBts: FestaBtsProps[] = [
- {
+  {
     id: 1,
     name: "Square Style",
     styles: [
@@ -9,31 +9,31 @@ export const festaBts: FestaBtsProps[] = [
         id: 1,
         name: "RM",
         image: "/Festa2025/namjoons_discharge_square_ver.webp",
-        color: "text-[#7167e6]"
+        color: "text-[#7167e6]",
       },
       {
         id: 2,
         name: "V",
         image: "/Festa2025/tae_s_discharge_square_ver.webp",
-        color: "text-[rgb(188,79,77)]"
+        color: "text-[rgb(188,79,77)]",
       },
       {
         id: 3,
         name: "Jimin",
         image: "/Festa2025/jimin_discharge_square_ver.webp",
-        color: "text-[rgb(188,79,77)]"
+        color: "text-[rgb(188,79,77)]",
       },
       {
         id: 4,
         name: "JungKook",
         image: "/Festa2025/jungkook discharge_square ver.webp",
-        color: "text-[rgb(195,61,122)]"
+        color: "text-[rgb(195,61,122)]",
       },
       {
         id: 5,
         name: "Suga",
-        image: "/HobiDischarge/hobissquare2.webp",
-        color: "text-[rgb(188,79,77)]",
+        image: "/Festa2025/yoongi_s dischage_square ver.webp",
+        color: "text-[#441e22]",
       },
     ],
   },
@@ -45,32 +45,32 @@ export const festaBts: FestaBtsProps[] = [
         id: 1,
         name: "RM",
         image: "/Festa2025/namjoons_discharge_mobile_ver.webp",
-        color: "text-[#7167e6]"
+        color: "text-[#7167e6]",
       },
       {
         id: 2,
         name: "V",
         image: "/Festa2025/tae_s_discharge_mobile_ver.webp",
-        color: "text-[rgb(188,79,77)]"
+        color: "text-[rgb(188,79,77)]",
       },
-       {
+      {
         id: 3,
         name: "Jimin",
         image: "/Festa2025/jimin_discharge_mobile_ver.webp",
-        color: "text-[rgb(188,79,77)]"
+        color: "text-[rgb(188,79,77)]",
       },
-       {
+      {
         id: 4,
         name: "JungKook",
         image: "/Festa2025/jungkook discharge_mobile ver.webp",
-        color: "text-[rgb(188,79,77)]"
+        color: "text-[rgb(188,79,77)]",
       },
-       {
+      {
         id: 5,
         name: "Suga",
-        image: "/HobiDischarge/hobisver2.webp",
-        color: "text-[rgb(188,79,77)]",
+        image: "/public/Festa2025/yoongi_s dischage_vertical ver.webp",
+        color: "text-[#441e22]",
       },
     ],
   },
-]
+];
